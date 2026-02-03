9905b018218dab510817153b56809588826091c3
osama45667 <vansarwanto@gmail.com>
2026-02-03 12:13:27 +0700

    Replace __PORT and  in nginx template

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 PORT_VALUE="${PORT:-80}"
-# Replace only the __PORT__ placeholder to keep nginx variables intact.
-sed "s/__PORT__/${PORT_VALUE}/g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Replace __PORT__ (preferred) and any leftover ${PORT} to avoid nginx errors.
+sed "s/__PORT__/${PORT_VALUE}/g; s/\\${PORT}/${PORT_VALUE}/g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 php-fpm -D
 nginx -g 'daemon off;'
