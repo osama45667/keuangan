c9691af164644525efb2d994ba444422baf802dd
osama45667 <vansarwanto@gmail.com>
2026-02-03 11:37:27 +0700

    Fix nginx template replace using sed

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 PORT_VALUE="${PORT:-80}"
-# Only substitute PORT to avoid wiping nginx variables like $document_root
-env PORT="$PORT_VALUE" envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Replace only the __PORT__ placeholder to keep nginx variables intact.
+sed "s/__PORT__/${PORT_VALUE}/g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 php-fpm -D
 nginx -g 'daemon off;'
