b81654f513b51272ae8e6a873d806dd49353d9d6
osama45667 <vansarwanto@gmail.com>
2026-02-03 20:19:30 +0700

    Fix trust proxies and safer defaults

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
@@ -19,5 +19,8 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = SymfonyRequest::HEADER_X_FORWARDED_FOR
+        | SymfonyRequest::HEADER_X_FORWARDED_HOST
+        | SymfonyRequest::HEADER_X_FORWARDED_PORT
+        | SymfonyRequest::HEADER_X_FORWARDED_PROTO;
 }
